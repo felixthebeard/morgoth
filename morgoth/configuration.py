1672263de3d9e8bf92196d2e2916fc0c79d94e33
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-06-25 23:07:49 +0200

    Add phys bkg to config

--- morgoth/configuration.py
+++ morgoth/configuration.py
@@ -6,6 +6,13 @@ structure = {}
 structure["pygcn"] = dict(port=8099)
 structure["luigi"] = dict(n_workers=4)
 structure["multinest"] = dict(n_cores=4, path_to_python="python")
+structure["phys_bkg"] = dict(
+    n_parallel_fits = 8,
+    multinest=dict(
+        n_cores=1,
+        path_to_python="python"
+    )
+)
 structure["download"] = dict(
     trigdat=dict(
         v00=dict(interval=5, max_time=1800),
