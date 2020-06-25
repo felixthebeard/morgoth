f8630288511a2da462c4bd7f8e0d5a8e5ed21e03
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-06-25 23:04:24 +0200

    Add phys bkg to reports

--- morgoth/reports.py
+++ morgoth/reports.py
@@ -16,6 +16,10 @@ class CreateAllPages(luigi.WrapperTask):
             "trigdat_v00": CreateReportTrigdat(grb_name=self.grb_name, version="v00"),
             "trigdat_v01": CreateReportTrigdat(grb_name=self.grb_name, version="v01"),
             "trigdat_v02": CreateReportTrigdat(grb_name=self.grb_name, version="v02"),
+
+            "trigdat_v00_phys_bkg": CreateReportTrigdat(grb_name=self.grb_name, version="v00", phys_bkg=True),
+            "trigdat_v01_phys_bkg": CreateReportTrigdat(grb_name=self.grb_name, version="v01", phys_bkg=True),
+            "trigdat_v02_phys_bkg": CreateReportTrigdat(grb_name=self.grb_name, version="v02", phys_bkg=True),
         }
 
 
@@ -44,19 +48,23 @@ class CreateReportTTE(luigi.Task):
 class CreateReportTrigdat(luigi.Task):
     grb_name = luigi.Parameter()
     version = luigi.Parameter(default="v00")
+    phys_bkg = luigi.BoolParameter()
 
     def requires(self):
         return {
             "report": UploadReport(
-                grb_name=self.grb_name, report_type="trigdat", version=self.version
+                grb_name=self.grb_name, report_type="trigdat", version=self.version, phys_bkg=self.phys_bkg
             ),
             "upload_all_plots": UploadAllPlots(
-                grb_name=self.grb_name, report_type="trigdat", version=self.version
+                grb_name=self.grb_name, report_type="trigdat", version=self.version, phys_bkg=self.phys_bkg
             ),
         }
 
     def output(self):
-        filename = f"trigdat_{self.version}_report_done.txt"
+        if self.phys_bkg:
+            filename = f"trigdat_{self.version}_phys_report_done.txt"
+        else:
+            filename = f"trigdat_{self.version}_report_done.txt"
         return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
 
     def run(self):
